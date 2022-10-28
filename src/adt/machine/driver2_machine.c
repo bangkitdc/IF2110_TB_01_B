5df3df3cc6b19b21f9120bdadccc9d12961a5930
bangkitdc <bangkitdc@gmail.com>
2022-10-29 03:42:18 +0700

    benerin batchfile

--- src/adt/machine/driver2_machine.c
+++ src/adt/machine/driver2_machine.c
@@ -3,15 +3,15 @@
 
 /* Driver input Console */
 
-int main() {
-    ListWord L;
-    createListWord(&L);
+// int main() {
+//     ListWord L;
+//     createListWord(&L);
 
-    L = readLine();
+//     L = readLine();
 
-    for (int i = 0; i < L.Length; i ++) {
-        displayWord(L.TabWords[i]);
-        printf(" ");
-    }
-    return 0;
-}
+//     for (int i = 0; i < L.Length; i ++) {
+//         displayWord(L.TabWords[i]);
+//         printf(" ");
+//     }
+//     return 0;
+// }
