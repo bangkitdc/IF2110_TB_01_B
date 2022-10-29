49a69e25daed7ed6e21a093bb4cfb59a450485c7
Myumet <16521017@mahasiswa.itb.ac.id>
2022-10-29 08:31:46 +0700

    add adt matrix and tree

--- src/adt/matrix/matrix.h
+++ src/adt/matrix/matrix.h
@@ -1,62 +1,56 @@
 /* ********** Definisi TYPE Matrix dengan Index dan elemen integer ********** */
-#include <stdio.h>
-#include "boolean.h"
-#include "matrix.h"
 
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "boolean.h"
+
+/* Ukuran maksimum baris dan kolom */
+#define ROW_CAP 100
+#define COL_CAP 100
+
+typedef int IdxType; /* Index baris, kolom */
+typedef char ElType;
+typedef struct
+{
+   ElType mem[ROW_CAP][COL_CAP];
+   int rowEff; /* banyaknya/ukuran baris yg terdefinisi */
+   int colEff; /* banyaknya/ukuran kolom yg terdefinisi */
+} Matrix;
 /* rowEff >= 1 dan colEff >= 1 */
 /* Indeks matriks yang digunakan: [0..ROW_CAP-1][0..COL_CAP-1] */
 /* Memori matriks yang dipakai selalu di "ujung kiri atas" */
 
 /* ********** DEFINISI PROTOTIPE PRIMITIF ********** */
 /* *** Konstruktor membentuk Matrix *** */
-void createMatrix(int nRows, int nCols, Matrix *m)
+void createMatrix(int nRows, int nCols, Matrix *m);
 /* Membentuk sebuah Matrix "kosong" yang siap diisi berukuran nRow x nCol di "ujung kiri" memori */
 /* I.S. nRow dan nCol adalah valid untuk memori matriks yang dibuat */
 /* F.S. Matriks m sesuai dengan definisi di atas terbentuk */
-{
-    ROW_EFF(*m) = nRows;
-    COL_EFF(*m) = nCols;
-}
+
+/* *** Selektor *** */
+#define ROW_EFF(M) (M).rowEff
+#define COL_EFF(M) (M).colEff
+#define ELMT(M, i, j) (M).mem[(i)][(j)]
 
 /* *** Selektor "Dunia Matrix" *** */
-boolean isMatrixIdxValid(int i, int j)
+boolean isMatrixIdxValid(int i, int j);
 /* Mengirimkan true jika i, j adalah index yang valid untuk matriks apa pun */
-{
-    return (i>=0 && i<ROW_CAP && j>=0 && j<COL_CAP);
-}
 
 /* *** Selektor: Untuk sebuah matriks m yang terdefinisi: *** */
-IdxType getLastIdxRow(Matrix m)
+IdxType getLastIdxRow(Matrix m);
 /* Mengirimkan Index baris terbesar m */
-{
-    return (IdxType) ROW_EFF(m)-1;
-}
-IdxType getLastIdxCol(Matrix m)
+IdxType getLastIdxCol(Matrix m);
 /* Mengirimkan Index kolom terbesar m */
-{
-    return (IdxType) COL_EFF(m)-1;
-}
-boolean isIdxEff(Matrix m, IdxType i, IdxType j)
+boolean isIdxEff(Matrix m, IdxType i, IdxType j);
 /* Mengirimkan true jika i, j adalah Index efektif bagi m */
-{
-    return (i>=0 && i<ROW_EFF(m) && j>=0 && j<COL_EFF(m));
-}
 
 /* ********** Assignment  Matrix ********** */
-void copyMatrix(Matrix mIn, Matrix *mOut)
+void copyMatrix(Matrix mIn, Matrix *mOut);
 /* Melakukan assignment mOut <- mIn */
-{
-    createMatrix(ROW_EFF(mIn), COL_EFF(mIn), mOut);
-    for(int i=0;i<ROW_EFF(mIn);i++){
-        for(int j=0;j<COL_EFF(mIn);j++){
-            ELMT(*mOut,i,j) = ELMT(mIn,i,j);
-        }
-    }
-}
 
 /* ********** KELOMPOK BACA/TULIS ********** */
-
-void displayMatrix(Matrix m)
+void displayMatrix(Matrix m);
 /* I.S. m terdefinisi */
 /* F.S. Nilai m(i,j) ditulis ke layar per baris per kolom, masing-masing elemen per baris 
    dipisahkan sebuah spasi. Baris terakhir tidak diakhiri dengan newline */
@@ -66,29 +60,13 @@ void displayMatrix(Matrix m)
 4 5 6
 8 9 10
 */
-{
-    for(int i=0;i<ROW_EFF(m);i++){
-        for(int j=0;j<COL_EFF(m);j++){
-            if(j>0){
-                printf(" ");
-            }
-            printf("%d", ELMT(m,i,j));
-        }
-        printf("\n");
-    }
-}
-
 
 /* ********** Operasi lain ********** */
-int countElmt(Matrix m)
+int countElmt(Matrix m);
 /* Mengirimkan banyaknya elemen m */
-{
-    return (ROW_EFF(m)*COL_EFF(m));
-}
 
 /* ********** KELOMPOK TEST TERHADAP Matrix ********** */
-boolean isSquare(Matrix m)
+boolean isSquare(Matrix m);
 /* Mengirimkan true jika m adalah matriks dg ukuran baris dan kolom sama */
-{
-    return (ROW_EFF(m) == COL_EFF(m));
-}
+
+#endif
