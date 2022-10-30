ddf8c44e2678e1c2abafd88f88073e08e44113b5
bangkitdc <bangkitdc@gmail.com>
2022-10-31 01:56:22 +0700

    config makanan DONE

--- src/adt/liststatik/liststatik.c
+++ src/adt/liststatik/liststatik.c
@@ -6,15 +6,15 @@
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create List kosong untuk list makanan */
 void createLSMakanan(ListStatik *l){
-    for (int i = 0; i < CAPACITY; i++){
-        ID(ELMT(*l,i)) = MARK;
+    for (int i = 0; i < CAPACITY_LISTSTATIK; i++){
+        ID(ELMT_LISTSTATIK(*l,i)) = MARKSTATIK;
     }
 }
 
 /* *** Banyaknya elemen *** */
-int listLength(ListStatik l){
+int listLengthStatik(ListStatik l){
     int i = 0;
-    while (ID(ELMT(l,i)) != MARK && i < CAPACITY){
+    while (ID(ELMT_LISTSTATIK(l,i)) != MARKSTATIK && i < CAPACITY_LISTSTATIK){
         i+=1;
     }
     return i;
@@ -29,18 +29,34 @@ dengan id makanan yang sudah ada dalam list sebelumnya. Jika Id nya sama dengan 
 maka food akan menimpa makanan yang sudah didefinisikan sebelumnya. */
     boolean cek = false;
     int i = 0;
-    while (i < listLength(*l) && cek == false){
-        if (ID(ELMT(*l,i)) == ID(food)) {
-            ELMT(*l,i) = food;
+    while (i < listLengthStatik(*l) && cek == false){
+        if (ID(ELMT_LISTSTATIK(*l,i)) == ID(food)) {
+            ELMT_LISTSTATIK(*l,i) = food;
             cek = true;
         }
         i += 1;
     }
     if (!cek) {
-        ELMT(*l, listLength(*l)) = food;
+        ELMT_LISTSTATIK(*l, listLengthStatik(*l)) = food;
     }
 }
 
-//ELMT(*l, listLength(*l)) = food;
+void printCatalog(ListStatik l) {
+    // KAMUS LOKAL
+    int i;
+
+    // ALGORITMA
+    printf("List Makanan\n");
+    printf("(nama - durasi kedaluwarsa - aksi yang diperlukan - delivery time)\n");
+
+    if (listLengthStatik(l) > 0) {
+        for (i = 0; i < listLengthStatik(l); i ++) {
+            Makanan temp = ELMT_LISTSTATIK(l, i);
+            printf("  %d. %s - %d:%d:%d - %c - %d:%d:%d\n", i + 1,NAME(temp), EXP(temp).DD, EXP(temp).HH, EXP(temp).MM, LOC(temp), DELIVERY(temp).DD, DELIVERY(temp).HH, DELIVERY(temp).MM);
+        }
+    }
+}
+
+//ELMT_LISTSTATIK(*l, listLengthStatik(*l)) = food;
 
 /* Konstruktor : create List kosong untuk list resep */
--- src/utility/game/game.h
+++ src/utility/game/game.h
@@ -2,14 +2,14 @@
 #define GAME_H
 
 #include <stdio.h>
-#include "../adt/adt.h"
+#include "../../adt/adt.h"
 
 int currentTime;
 
 typedef enum {
     MAKANAN = 1,
-    RESEP = 2;
-    PETA = 3;
+    RESEP = 2,
+    PETA = 3
 } ConfigType;
 
 typedef struct {
